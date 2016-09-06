dea717b7e2e951bd458c4278e320909ddb1fe409
Sam Xu <saxu@microsoft.com>
2016-09-06 18:27:05 +0800

    add test cases

---
+++ src/DependencyInjection/DependencyInjectionTest/src/DependencyInjectionTest/ConstructorDependencyTest.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace DependencyInjectionTest
+{
+    public class ConstructorDependencyTest
+    {
+        private readonly ITestOutputHelper output;
+
+        public ConstructorDependencyTest(ITestOutputHelper op)
+        {
+            output = op;
+        }
+
+        [Fact]
+        public void ConstructorDependencyTest_Failed()
+        {
+            IServiceCollection services = new ServiceCollection();
+     //       services.AddSingleton(typeof(IMyInterface), typeof(MyClass));
+            services.AddScoped<ConstructorDependency>();
+
+            IServiceProvider provider = services.BuildServiceProvider();
+
+            InvalidOperationException exception = Assert.Throws<InvalidOperationException>(() => provider.GetRequiredService<ConstructorDependency>());
+            // output.WriteLine(exception.Message);
+            Assert.Equal("Unable to resolve service for type 'DependencyInjectionTest.IMyInterface' while attempting to activate 'DependencyInjectionTest.ConstructorDependency'.", exception.Message);
+        }
+
+        [Fact]
+        public void ConstructorDependencyTest_Successed()
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.AddSingleton(typeof(IMyInterface), typeof(MyClass));
+            services.AddScoped<ConstructorDependency>();
+
+            IServiceProvider provider = services.BuildServiceProvider();
+
+            ConstructorDependency c = provider.GetRequiredService<ConstructorDependency>();
+
+            Assert.NotNull(c);
+        }
+
+        [Fact]
+        public void ConstructorDependencyTest_Failed_ForInt()
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.AddSingleton(typeof(IMyInterface), typeof(MyClass));
+            services.AddScoped<ConstructorDependency2>();
+
+            IServiceProvider provider = services.BuildServiceProvider();
+
+            InvalidOperationException exception = Assert.Throws<InvalidOperationException>(() => provider.GetRequiredService<ConstructorDependency2>());
+
+            Assert.Equal("Unable to resolve service for type 'System.Int32' while attempting to activate 'DependencyInjectionTest.ConstructorDependency2'.", exception.Message);
+        }
+
+        [Fact]
+        public void ConstructorDependencyTest_Successed_Multiple()
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.AddSingleton(typeof (IMyInterface), typeof (MyClass));
+            services.AddSingleton(typeof (IOtherInteface), typeof (OtherClass));
+            services.AddScoped<ConstructorDependency3>();
+
+            IServiceProvider provider = services.BuildServiceProvider();
+
+            ConstructorDependency3 c = provider.GetRequiredService<ConstructorDependency3>();
+
+            Assert.NotNull(c);
+        }
+    }
+
+    public interface IMyInterface
+    {
+        
+    }
+
+    public class MyClass : IMyInterface
+    {
+        public MyClass()
+        {
+            Console.WriteLine("In MyClass Constructor!");
+        }
+    }
+
+    public class ConstructorDependency
+    {
+        private IMyInterface _my;
+
+        public ConstructorDependency(IMyInterface my)
+        {
+            _my = my;
+
+            Console.WriteLine("In ConstructorDependency Constructor!");
+        }
+    }
+
+    public class ConstructorDependency2
+    {
+        private IMyInterface _my;
+
+        public ConstructorDependency2(IMyInterface my, int otherData)
+        {
+            _my = my;
+
+            Console.WriteLine("In ConstructorDependency2 Constructor!");
+        }
+    }
+
+    public interface IOtherInteface
+    { }
+
+    public class OtherClass : IOtherInteface
+    {
+    }
+
+
+    public class ConstructorDependency3
+    {
+        private IMyInterface _my;
+
+        public ConstructorDependency3(IMyInterface my, IOtherInteface otherData)
+        {
+            _my = my;
+
+            Console.WriteLine("In ConstructorDependency3 Constructor!");
+        }
+    }
+}
--- src/DependencyInjection/DependencyInjectionTest/src/DependencyInjectionTest/Program.cs
+++ src/DependencyInjection/DependencyInjectionTest/src/DependencyInjectionTest/Program.cs
@@ -2,13 +2,48 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace DependencyInjectionTest
 {
     public class Program
     {
+        private static int num = 0;
+
+        private int nonStatic = 0;
+
+        public Program()
+        {
+            num++;
+            nonStatic = 999;
+        }
+
         public static void Main(string[] args)
         {
+            IServiceCollection services = new ServiceCollection();
+            services.AddSingleton<Program>();
+
+            IServiceProvider provider = services.BuildServiceProvider();
+            Program p = provider.GetRequiredService<Program>();
+
+            Console.WriteLine("" + Program.num);
+            Console.WriteLine("" + p.nonStatic);
+
+            p = provider.GetRequiredService<Program>();
+            Console.WriteLine("" + Program.num);
+            Console.WriteLine("" + p.nonStatic);
+
+            services.AddScoped<Program>();
+
+            provider = services.BuildServiceProvider();
+            p = provider.GetRequiredService<Program>();
+
+            Console.WriteLine("" + Program.num);
+            Console.WriteLine("" + p.nonStatic);
+
+            p = provider.GetRequiredService<Program>();
+            Console.WriteLine("" + Program.num);
+            Console.WriteLine("" + p.nonStatic);
         }
     }
 }
