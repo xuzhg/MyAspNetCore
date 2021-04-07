9bd6fd4071a469d4373761f4d342ed263487465e
Sam Xu <saxu@microsoft.com>
2021-04-07 11:38:28 -0700

    Add HandleOthersController

---
+++ src/AttributeRouting80Rc/AttributeRouting80Rc/Controllers/HandleOthersController.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Routing.Attributes;
+
+namespace AttributeRouting80Rc.Controllers
+{
+    public class HandleOthersController : Controller
+    {
+        [ODataRouting]
+        [HttpGet("odata/Orders/{key}")]
+        public IActionResult Get(int key)
+        {
+            return Ok($"Orders{key} from OData");
+        }
+
+        [HttpGet("odata/Orders({key})")]
+        public IActionResult GetOrder(int key)
+        {
+            return Ok($"Orders{key} from non-OData");
+        }
+    }
+}
