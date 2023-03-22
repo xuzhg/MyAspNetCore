6cc2fb559db2b5b0013c515d84132a88ee945da7
Sam Xu <saxu@microsoft.com>
2023-03-22 11:43:25 -0700

    Add the missing using for gRPC client

--- src/gRPC.OData/gRPC.OData.Client/ODataBookstoreClient.cs
+++ src/gRPC.OData/gRPC.OData.Client/ODataBookstoreClient.cs
@@ -4,6 +4,7 @@ using Grpc.Net.Client;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Text.Json;
